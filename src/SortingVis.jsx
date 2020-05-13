25fefd6b10a7d02b84f98f7fd1b9b929efe52e01
adamf0x <adamfox96@gmail.com>
2020-05-12 23:36:20 -0700

    attempted to add animations to sorting

--- src/SortingVis.jsx
+++ src/SortingVis.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import "./SortingVis.css"
+import "./SortingVis.css";
 
-export class SortingVis extends React.Component{
+export class SortingVis extends React.Component {
   constructor(props) {
     super(props);
 
@@ -11,49 +11,65 @@ export class SortingVis extends React.Component{
     };
   }
 
-  componentDidMount(){
-        this.generateArray();
+  componentDidMount() {
+    this.generateArray();
   }
   generateArray() {
     var max = 100;
     var min = 10;
     var arr = [];
-    for(let i = 0; i < 100; i++){
-        arr.push(Math.floor(Math.random() * (max - min + 1) + min));
+    for (let i = 0; i < 100; i++) {
+      arr.push(Math.floor(Math.random() * (max - min + 1) + min));
     }
-    this.setState({array: arr});
+    this.setState({ array: arr });
   }
 
-  bubbleSort(){
+  bubbleSort() {
     let arr = this.state.array;
-    for(let i = 1; i < arr.length; i++){
-      for(let j = 0; j < i; j++){
-        if(arr[i] < arr[j]){
+    for (let i = 1; i < arr.length; i++) {
+      setTimeout(()=>{
+      for (let j = 0; j < i; j++) {
+        setTimeout(()=>{
+        if (arr[i] < arr[j]) {
+          let ball1 = document.getElementById(i);
+          let ball2 = document.getElementById(j);
+          ball1.style.backgroundColor = "red";
+          ball2.style.backgroundColor = "red";
           let temp = arr[i];
           arr[i] = arr[j];
           arr[j] = temp;
+          this.setState({ array: arr });
         }
+      }, i*50)
+      
       }
+    }, i*50) 
     }
-    this.setState({array: arr});
   }
-  
-  render(){
-      const {array} = this.state;
-      return(
-          <div className = 'container'>
-             <div className = 'buttons'>
-              <button onClick = {() => this.generateArray()}>reset array</button>
-              <button onClick = {() => this.bubbleSort()}>sort</button>
-          </div>
-          {array.map((value, index) =>(
-                  <div className = 'bar' key = {index} style = {{'height': `${value}px`, 'width': `${value}px`}}>
-                      {value}
-                  </div>
-              ))}
-          </div>
-      )
-  }
-  
-}
 
+  render() {
+    const { array } = this.state;
+    return (
+      <div className="container">
+        <div className="buttons">
+          <button className="action" onClick={() => this.generateArray()}>
+            reset array
+          </button>
+          <button className="action" onClick={() => this.bubbleSort()}>
+            visualize sorting
+          </button>
+        </div>
+        {array.map((value, index) => (
+          <div
+            className="bar"
+            key={index}
+            id = {index}
+            style={{height: `${value}px`, width: `${value}px`}}
+          >
+            {value}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}

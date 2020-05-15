644b99971e93384bfc6b4fb3c0f4b4e8b7022221
adamf0x <adamfox96@gmail.com>
2020-05-15 00:52:30 -0700

    added slider to select number of elements to sort

--- src/SortingVis.jsx
+++ src/SortingVis.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./App.css";
 import "./SortingVis.css";
 import { bubbleSortAnimations } from "./GetBubbleSortAnimations";
-var numBars = 20;
+import { Slider } from "@material-ui/core";
+
+var numBars = 100;
 var barWidth = Math.floor(1000 * (1 / numBars));
 
 export class SortingVis extends React.Component {
@@ -15,9 +17,9 @@ export class SortingVis extends React.Component {
   }
 
   componentDidMount() {
-    this.generateArray();
+    this.generateArray(numBars);
   }
-  generateArray() {
+  generateArray(numBars) {
     var max = numBars * 3;
     var min = 5;
     var arr = [];
@@ -28,8 +30,7 @@ export class SortingVis extends React.Component {
   }
 
   bubbleSort() {
-    var delay = Math.floor(1000*(1/(numBars + 10)));
-    console.log(delay)
+    var delay = Math.floor(1000 * (1 / (numBars + 10)));
     const animations = bubbleSortAnimations(this.state.array);
     for (let i = 0; i < animations.length; i++) {
       const elems = document.getElementsByClassName("bar");
@@ -37,8 +38,8 @@ export class SortingVis extends React.Component {
       if (i % 3 !== 2) {
         const bar1 = elems[animations[i][0]];
         const bar2 = elems[animations[i][1]];
-        //setting the elements to alternate colour 
-        if (i % 3 == 0) {
+        //setting the elements to alternate colour
+        if (i % 3 === 0) {
           setTimeout(() => {
             bar1.style.backgroundColor = "red";
             bar2.style.backgroundColor = "red";
@@ -52,13 +53,13 @@ export class SortingVis extends React.Component {
         }
         //if the loop is on an element that signifies a swap of two bars in the array, swap their heights
       } else {
-        if (animations[i].length != 0) {
+        if (animations[i].length !== 0) {
           setTimeout(() => {
             var bar1 = elems[animations[i][0]];
             var bar2 = elems[animations[i][1]];
-            var temp = bar1.style.height 
-            bar1.style.height = `${bar2.style.height}`
-            bar2.style.height = `${temp}`
+            var temp = bar1.style.height;
+            bar1.style.height = `${bar2.style.height}`;
+            bar2.style.height = `${temp}`;
           }, i * delay);
         }
       }
@@ -70,19 +71,30 @@ export class SortingVis extends React.Component {
     return (
       <div>
         <div className="buttons">
-          <button className="action" onClick={() => this.generateArray()}>
-            reset array
+          <button className="action" onClick={() => this.generateArray(numBars)}>
+            generate array
           </button>
           <button className="action" onClick={() => this.bubbleSort()}>
             visualize sorting
           </button>
+          <div className="slider">
+            <div className = 'sliderText'>Number of array elements/sorting speed:</div>
+            <Slider
+              min={0}
+              step={1}
+              max={300}
+              onChange = {(e, num)=>{numBars = num; barWidth = Math.floor(1000 * (1 / numBars)); this.generateArray(numBars)}}
+              defaultValue={100}
+              valueLabelDisplay="auto"
+              aria-labelledby="non-linear-slider"
+            ></Slider>
+          </div>
         </div>
         <div className="container">
           {array.map((value, index) => (
             <div
               className="bar"
               key={index}
-              id={index}
               style={{ height: `${value}px`, width: `${barWidth}px` }}
             >
               {value}

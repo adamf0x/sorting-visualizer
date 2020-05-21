1daa3dfd39eaa89e080fea067d54550e7d99b5a3
adamf0x <adamfox96@gmail.com>
2020-05-20 20:43:44 -0700

    Updates

---
+++ precache-manifest.7e4a24315fcba982dc43cf8ff6cff5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fc3668f830f67c5786d44ee632351d",
+    "url": "/sorting-visualizer/index.html"
+  },
+  {
+    "revision": "9b0663c8fcd796dd0b39",
+    "url": "/sorting-visualizer/static/css/main.03097f4a.chunk.css"
+  },
+  {
+    "revision": "b9ed15061ac9271310ce",
+    "url": "/sorting-visualizer/static/js/2.63926436.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/sorting-visualizer/static/js/2.63926436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0663c8fcd796dd0b39",
+    "url": "/sorting-visualizer/static/js/main.9f03c369.chunk.js"
+  },
+  {
+    "revision": "f8a77ab9ffd15119d5db",
+    "url": "/sorting-visualizer/static/js/runtime-main.5cb7f1f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.18139cb036cf4c7cb9f54bf0992ebadb.js"
+  "/sorting-visualizer/precache-manifest.7e4a24315fcba982dc43cf8ff6cff5d3.js"
 );
 
 self.addEventListener('message', (event) => {
